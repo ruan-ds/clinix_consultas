4299cb3ecc1571f031ba07629fc4e558217330e2
ruan-ds <ruantmelon@icloud.com>
2026-03-20 00:44:31 -0300

    refactor: replace declarative_base() with DeclarativeBase and add naming convention for Alembic migrations

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -1,13 +1,20 @@
 import os
 
-from sqlalchemy.orm import declarative_base
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from dotenv import load_dotenv
 
 
-Base = declarative_base()
+POSTGRES_NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",  
+    "uq": "uq_%(table_name)s_%(column_0_name)s",  
+    "ck": "ck_%(table_name)s_%(constraint_name)s",  
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",  
+    "pk": "pk_%(table_name)s"
+}
 
+class Base(DeclarativeBase):
+    metadata = MetaData(naming_convention=POSTGRES_NAMING_CONVENTION)
 
 
 load_dotenv()
@@ -24,8 +31,6 @@ engine = create_engine(
 
 # Cria a fabrica de sessões
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
