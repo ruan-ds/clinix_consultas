69f8ed43a462bdee5d68f7a9ab3ffe85c8be4044
ruan-ds <ruantmelon@icloud.com>
2026-03-22 03:17:42 -0300

    refactor: remove redundant index flags from models

--- backend/app/models/clinic.py
+++ backend/app/models/clinic.py
@@ -13,7 +13,8 @@ class Clinic(Entity):
     __tablename__ = "clinic"
 
     id = Column(Integer, ForeignKey("entity.id"), primary_key=True)
-    name = Column(String(50), nullable=False, unique=True)
+    trade_name = Column(String(60), nullable=False)
+    legal_name = Column(String(90), nullable=False, unique=True)
     cnpj = Column(String(14), nullable=False, unique=True)
     address_id = Column(Integer, ForeignKey("address.id"), nullable=False)
     is_active = Column(Boolean, default=True)
--- backend/app/models/phone.py
+++ backend/app/models/phone.py
@@ -9,5 +9,6 @@ class Phone(Base):
     id = Column(Integer, primary_key=True)
     entity_id = Column(Integer, ForeignKey("entity.id"), nullable=False)
     phone = Column(String(11), nullable=False)
+    type = Column(String(50))
 
     entity = relationship("Entity", back_populates="phones")
