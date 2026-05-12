2e4dcdf582d8c3b3b5c7e57d0605d4fc515c02a1
ruan-ds <ruantmelon@icloud.com>
2026-05-11 23:52:02 -0300

    refactor(models): add new fields and relationships to Service model

--- backend/app/models/service.py
+++ backend/app/models/service.py
@@ -3,6 +3,9 @@ from sqlalchemy import (
     Column,
     Integer,
     String,
+    Numeric,
+    ForeignKey,
+    Boolean
 )
 from app.core.base_model import Base
 
@@ -11,7 +14,10 @@ class Service(Base):
     __tablename__ = "service"
 
     id = Column(Integer, primary_key=True)
-    role = Column(String(15), nullable=False)
+    clinic_id = Column(Integer, ForeignKey("clinic.id"), nullable=False)
+    name = Column(String(100), nullable=False)
+    price = Column(Numeric(10, 2), nullable=False)
+    is_active = Column(Boolean, default=True)
 
-    clinical_access = relationship("ClinicalAccess", back_populates="specialtie")
-    medical_appointment = relationship("MedicalAppointment", back_populates="service")
+    clinic = relationship("Clinic", back_populates="services")
+    medical_appointments = relationship("MedicalAppointment", back_populates="service")
