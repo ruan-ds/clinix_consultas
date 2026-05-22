db0c0d2c5c160e6333f8be050dc1d9dd7575cc2b
dev-pabloF <pablofelipeb21@gmail.com>
2026-05-21 21:13:30 -0300

    feat(doctors): update doctors component and add styles

--- frontend/src/components/feed/content/content.tsx
+++ frontend/src/components/feed/content/content.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Dashboard from './dashboard/dashboard';
 import Appointment from './appointment/appointment';
+import Doctors from './doctors/doctors';
 import './content.css';
 
 function Content() { 
     return ( 
         <div className="content">
-            <Appointment />
+            <Doctors />
         </div>
     )
 
