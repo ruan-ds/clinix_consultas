88c833d900949dc01b413d336a16bab1f3723a0d
dev-pabloF <pablofelipeb21@gmail.com>
2026-05-21 21:21:21 -0300

    feat(configs): update configs component and styles

--- frontend/src/components/feed/content/content.tsx
+++ frontend/src/components/feed/content/content.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Dashboard from './dashboard/dashboard';
 import Appointment from './appointment/appointment';
 import Doctors from './doctors/doctors';
+import Configs from './configs/configs';
 import './content.css';
 
 function Content() { 
     return ( 
         <div className="content">
-            <Doctors />
+            <Configs />
         </div>
     )
 
