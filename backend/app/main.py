691ad77f452504d292d76e1a4bed13ce8d24012d
Bernardo-Policarpo <bernardopolicarpo061107@gmail.com>
2026-06-02 19:09:18 -0300

    feat(main): patient router included

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from app.api.public.registration import router as registration_router
 from app.api.public.login import router as login_router
+from app.api.public.patient import router as patient_router
 from app.api.clinical.medical_appointment import router as appointment_router
 
 from fastapi import FastAPI
@@ -25,6 +26,7 @@ app.add_middleware(
 
 app.include_router(registration_router)
 app.include_router(login_router)
+app.include_router(patient_router)
 app.include_router(appointment_router)
 
 
